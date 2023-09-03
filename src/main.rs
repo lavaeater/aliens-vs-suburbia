54cb14b09909ec259abc402c4d5ab865bacd720b
Tommie Nygren <tommie.nygren@gmail.com>
2023-09-03 09:33:14 +0200

    Adds startup of bevy app Adds system to load simple player model

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,19 @@
+use bevy::app::{App, Startup};
+use bevy::DefaultPlugins;
+use bevy::prelude::Msaa;
+use bevy_xpbd_3d::plugins::PhysicsPlugins;
+use crate::player::systems::spawn_players::spawn_players;
+
 pub(crate) mod player;
 pub(crate) mod general;
 
 fn main() {
-
+    App::new()
+        .insert_resource(Msaa::Sample4)
+        .add_plugins(DefaultPlugins)
+        .add_plugins(PhysicsPlugins::default())
+        .add_systems(Startup, (
+            spawn_players,
+))
+        .run();
 }
--- src/player/systems/spawn_players.rs
+++ src/player/systems/spawn_players.rs
@@ -1,7 +1,19 @@
 use bevy::asset::AssetServer;
-use bevy::prelude::{Commands, Res};
+use bevy::prelude::{Commands, Res, Transform};
+use bevy::scene::SceneBundle;
 
 pub fn spawn_players(
     mut commands: Commands,
     asset_server: Res<AssetServer>
-) {}
+) {
+    // note that we have to include the `Scene0` label
+    let my_gltf = asset_server.load("my.glb#Scene0");
+
+    // to position our 3d model, simply use the Transform
+    // in the SceneBundle
+    commands.spawn(SceneBundle {
+        scene: my_gltf,
+        transform: Transform::from_xyz(2.0, 0.0, -5.0),
+        ..Default::default()
+    });
+}
