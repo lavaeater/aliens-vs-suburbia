850d2ab3d37d1d3a63d220fe08590fed2b41e58e
Tommie Nygren <tommie.nygren@gmail.com>
2023-09-14 14:19:00 +0200

    Super simple collider added instead of fiddling with generating the collider as I did before

--- src/player/systems/spawn_players.rs
+++ src/player/systems/spawn_players.rs
@@ -6,29 +6,13 @@ use bevy_xpbd_3d::prelude::Collider;
 
 pub fn spawn_players(
     mut commands: Commands,
-    gltf_assets: Res<Assets<Gltf>>,
-    gltf_mesh_assets: Res<Assets<GltfMesh>>,
-    mesh_assets: Res<Assets<Mesh>>,
     asset_server: Res<AssetServer>,
 ) {
-    let gltf = asset_server.load("player.glb#Scene0");
+    commands.spawn((SceneBundle {
+        scene: asset_server.load("player.glb#Scene0"),
+        transform: Transform::from_xyz(2.0, 0.0, -5.0),
 
-
-    if let Some(actual_gltf) = gltf_assets.get(&gltf) {
-        let mesh = gltf_mesh_assets.get(actual_gltf.meshes.first().unwrap()).unwrap();
-        let mesh_mesh = mesh_assets.get(&mesh.primitives.first().unwrap().mesh).unwrap().clone();
-        let scene = actual_gltf.scenes.first().unwrap();
-
-        let collider = Collider::convex_decomposition_from_bevy_mesh(&mesh_mesh).unwrap();
-
-        // to position our 3d model, simply use the Transform
-        // in the SceneBundle
-        commands.spawn((
-            SceneBundle {
-                scene: scene.clone(),
-                transform: Transform::from_xyz(0.0, 0.0, 0.0),
-                ..Default::default()
-            },
-            collider));
-    }
+        ..Default::default()
+    },
+    Collider::cuboid(0.5,0.5,0.3)));
 }
