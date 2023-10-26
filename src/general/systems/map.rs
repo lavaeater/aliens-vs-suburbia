1885e9b0f5316cd4bc2f1bfa47293630e38b9fcd
Tommie Nygren <tommie.nygren@gmail.com>
2023-10-26 16:00:38 +0200

    Some work being done on the map stuff

--- src/general/systems/map.rs
+++ src/general/systems/map.rs
@@ -1,7 +1,7 @@
 use bevy::asset::AssetServer;
 use bevy::core::Name;
 use bevy::math::Vec3;
-use bevy::prelude::{Commands, Res};
+use bevy::prelude::{Commands, Res, Transform};
 use bevy::scene::SceneBundle;
 use bevy_xpbd_3d::prelude::{Collider, Position, RigidBody};
 use flagset::{flags, Flags, FlagSet};
@@ -41,8 +41,8 @@ pub fn spawn_map(
 ) {
     let min_x = -1;
     let max_x = 1;
-    let min_y = -3;
-    let max_y = 3;
+    let min_y = -1;
+    let max_y = 1;
     let ts = (min_x..=max_x).map(|x| (min_y..=max_y).map(|y| {
         let mut flags = MapTile::new(TileFlags::Floor);
         if y == min_y {
@@ -90,8 +90,8 @@ pub fn spawn_map(
                         ..Default::default()
                     },
                     RigidBody::Static,
-                    Collider::cuboid(2.0, 0.125, 2.0),
-                    Position::from(Vec3::new(2.0 * x as f32, -2.0, 2.0 * y as f32)),
+                    Collider::cuboid(0.125, 2.0, 2.0),
+                    Position::from(Vec3::new(2.0 * x as f32 + 1.0, -2.0, 2.0 * y as f32)),
                 ));
             }
             if tile.0.contains(TileFlags::WallSouth) {
@@ -99,11 +99,13 @@ pub fn spawn_map(
                     Name::from(format!("Wall South {}:{}", x, y)),
                     SceneBundle {
                         scene: asset_server.load("wall_south.glb#Scene0"),
+                        transform: Transform::from_xyz(2.0 * x as f32 - 1.0, -2.0, 2.0 * y as f32),
                         ..Default::default()
                     },
                     RigidBody::Static,
-                    Collider::cuboid(2.0, 0.125, 2.0),
-                    Position::from(Vec3::new(2.0 * x as f32, -2.0, 2.0 * y as f32)),
+                    Collider::cuboid(0.125, 2.0, 2.0),
+                    Position::from(Vec3::new(2.0 * x as f32 - 1.0, -2.0, 2.0 * y as f32)),
+
                 ));
             }
             if tile.0.contains(TileFlags::WallEast) {
@@ -114,7 +116,7 @@ pub fn spawn_map(
                         ..Default::default()
                     },
                     RigidBody::Static,
-                    Collider::cuboid(2.0, 0.125, 2.0),
+                    Collider::cuboid(0.10, 0.10, 0.10),
                     Position::from(Vec3::new(2.0 * x as f32, -2.0, 2.0 * y as f32)),
                 ));
             }
@@ -126,7 +128,7 @@ pub fn spawn_map(
                         ..Default::default()
                     },
                     RigidBody::Static,
-                    Collider::cuboid(2.0, 0.125, 2.0),
+                    Collider::cuboid(0.10, 0.10, 0.10),
                     Position::from(Vec3::new(2.0 * x as f32, -2.0, 2.0 * y as f32)),
                 ));
             }
