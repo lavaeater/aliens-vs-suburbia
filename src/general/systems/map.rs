9c76531364fa0e1fc529511435c683a585481b76
Tommie Nygren <tommie.nygren@gmail.com>
2023-10-26 15:45:32 +0200

    Can draw a room, dude.

--- src/general/systems/map.rs
+++ src/general/systems/map.rs
@@ -4,27 +4,125 @@ use bevy::math::Vec3;
 use bevy::prelude::{Commands, Res};
 use bevy::scene::SceneBundle;
 use bevy_xpbd_3d::prelude::{Collider, Position, RigidBody};
+use flagset::{flags, Flags, FlagSet};
+
+flags! {
+    enum TileFlags: u64 {
+        Floor,
+        WallNorth,
+        WallEast,
+        WallSouth,
+        WallWest,
+        Pickup,
+        PossibleEncounter
+    }
+}
+
+/*
+
+ */
+pub struct MapTile(FlagSet<TileFlags>);
+
+impl MapTile {
+    fn new(flags: impl Into<FlagSet<TileFlags>>) -> MapTile {
+        MapTile(flags.into())
+    }
+}
+
+pub struct MapDef {
+    pub x: i32,
+    pub y: i32,
+    pub tiles: Vec<Vec<MapTile>>,
+}
 
 pub fn spawn_map(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
 ) {
-    //create a super simple array that is the map.
-    let map_array: [[bool; 5];5] = [
-        [true, true, false, false, false],
-        [false, true, false, false, false],
-        [false, true, true, true, true],
-        [false, true, true, false, false],
-        [false, false, true, false, false]
-    ];
+    let min_x = -1;
+    let max_x = 1;
+    let min_y = -3;
+    let max_y = 3;
+    let ts = (min_x..=max_x).map(|x| (min_y..=max_y).map(|y| {
+        let mut flags = MapTile::new(TileFlags::Floor);
+        if y == min_y {
+            flags.0 ^= TileFlags::WallSouth
+        }
+        if y == max_y {
+            flags.0 ^= TileFlags::WallNorth
+        }
 
-    for (y, row) in map_array.iter().enumerate() {
-        for (x, column) in row.iter().enumerate() {
-            if *column {
+        if x == max_x {
+            flags.0 ^= TileFlags::WallEast
+        }
+        if x == min_x {
+            flags.0 ^= TileFlags::WallWest
+        }
+        flags
+    }).collect()
+    ).collect();
+
+    let map = MapDef {
+        x: -5,
+        y: -5,
+        tiles: ts,
+    };
+
+    for (y, row) in map.tiles.iter().enumerate() {
+        for (x, tile) in row.iter().enumerate() {
+            if tile.0.contains(TileFlags::Floor) {
                 commands.spawn((
-                    Name::from(format!("Tile {}:{}", x, y)),
+                    Name::from(format!("Floor {}:{}", x, y)),
                     SceneBundle {
                         scene: asset_server.load("floor_fab.glb#Scene0"),
+                        ..Default::default()
+                    },
+                    RigidBody::Static,
+                    Collider::cuboid(2.0, 0.125, 2.0),
+                    Position::from(Vec3::new(2.0 * x as f32, -2.0, 2.0 * y as f32)),
+                ));
+            }
+            if tile.0.contains(TileFlags::WallNorth) {
+                commands.spawn((
+                    Name::from(format!("Wall North {}:{}", x, y)),
+                    SceneBundle {
+                        scene: asset_server.load("wall_north.glb#Scene0"),
+                        ..Default::default()
+                    },
+                    RigidBody::Static,
+                    Collider::cuboid(2.0, 0.125, 2.0),
+                    Position::from(Vec3::new(2.0 * x as f32, -2.0, 2.0 * y as f32)),
+                ));
+            }
+            if tile.0.contains(TileFlags::WallSouth) {
+                commands.spawn((
+                    Name::from(format!("Wall South {}:{}", x, y)),
+                    SceneBundle {
+                        scene: asset_server.load("wall_south.glb#Scene0"),
+                        ..Default::default()
+                    },
+                    RigidBody::Static,
+                    Collider::cuboid(2.0, 0.125, 2.0),
+                    Position::from(Vec3::new(2.0 * x as f32, -2.0, 2.0 * y as f32)),
+                ));
+            }
+            if tile.0.contains(TileFlags::WallEast) {
+                commands.spawn((
+                    Name::from(format!("Wall East {}:{}", x, y)),
+                    SceneBundle {
+                        scene: asset_server.load("wall_east.glb#Scene0"),
+                        ..Default::default()
+                    },
+                    RigidBody::Static,
+                    Collider::cuboid(2.0, 0.125, 2.0),
+                    Position::from(Vec3::new(2.0 * x as f32, -2.0, 2.0 * y as f32)),
+                ));
+            }
+            if tile.0.contains(TileFlags::WallWest) {
+                commands.spawn((
+                    Name::from(format!("Wall West {}:{}", x, y)),
+                    SceneBundle {
+                        scene: asset_server.load("wall_west.glb#Scene0"),
                         ..Default::default()
                     },
                     RigidBody::Static,
