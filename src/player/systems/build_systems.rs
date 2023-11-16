d70e27d449c20ead0c75c746e4ba16659ec61515
Tommie Nygren <tommie.nygren@gmail.com>
2023-11-16 21:36:12 +0100

    I have a working idea of how this should work inside my brain.
    Cool.
    
    Here we goo!

--- src/player/systems/build_systems.rs
+++ src/player/systems/build_systems.rs
@@ -9,6 +9,7 @@ use bevy_xpbd_3d::prelude::Position;
 use crate::general::components::CollisionLayer;
 use crate::general::components::map_components::CurrentTile;
 use crate::general::resources::map_resources::MapGraph;
+use crate::general::systems::map_systems::TileDefinitions;
 use crate::player::components::general::{BuildingIndicator, IsBuilding};
 use crate::player::events::building_events::{StartBuilding, StopBuilding};
 
@@ -16,22 +17,9 @@ use crate::player::events::building_events::{StartBuilding, StopBuilding};
 pub fn start_build(
     mut start_evr: EventReader<StartBuilding>,
     mut commands: Commands,
-    asset_server: Res<AssetServer>
 ) {
     for start_event in start_evr.read() {
         commands.entity(start_event.0).insert(IsBuilding {});
-        // let entity_commands = commands.spawn((
-        //     Name::from("BuildingIndicator"),
-        //     SceneBundle {
-        //         scene: asset_server.load("floor_fab.glb#Scene0"),
-        //         ..Default::default()
-        //     },
-        //     RigidBody::Kinematic,
-        //     Collider::cuboid(tile_width, tile_depth, tile_width),
-        //     Position::from(Vec3::new(tile_width * tile.x as f32, -2.0, tile_width * tile.y as f32)),
-        //     CollisionLayers::new([CollisionLayer::BuildIndicator], []),
-        //     CurrentTile::default(),
-        // ));
     }
 }
 
@@ -45,40 +33,46 @@ pub fn stop_build(
 }
 
 pub fn building_mode(
-    mut builder_query: Query<(&CurrentTile, &Rotation, &Position), With<IsBuilding>>,
+    mut builder_query: Query<(&CurrentTile, &Rotation, &Position, Has<BuildingIndicator>), With<IsBuilding>>,
     map_graph: Res<MapGraph>,
     mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    tile_definitions: Res<TileDefinitions>,
 ) {
-    for (current_tile, rotation, position) in builder_query.iter_mut() {
-        let neighbours = map_graph.grid.neighbours(current_tile.tile);
-        /*
-        Figure out how to "point" att one of the neighbours.
-         */
-        let player_direction =
-            rotation.0.
-                mul_vec3(Vec3::new(0.0, 0.0, -1.0))
-                .xz()
-                .normalize()
-                .to_grid_neighbour();
+    for (current_tile, rotation, position, has_indicator) in builder_query.iter_mut() {
+        if has_indicator {
+            if map_graph.grid.has_vertex(desired_neighbour) {}
+        } else {
+            let entity_commands = commands.spawn((
+                Name::from("BuildingIndicator"),
+                SceneBundle {
+                    scene: asset_server.load("floor_fab.glb#Scene0"),
+                    ..Default::default()
+                },
+                RigidBody::Kinematic,
+                tile_definitions.create_floor_collider(),
+                Position::from(Vec3::new(tile_width * tile.x as f32, -2.0, tile_width * tile.y as f32)),
+                CollisionLayers::new([CollisionLayer::BuildIndicator], []),
+                CurrentTile::default(),
+            ));
+        }
 
-        let desired_neighbour = ((current_tile.tile.0 as i32 + player_direction.0) as usize, (current_tile.tile.1 as i32 + player_direction.1) as usize);
-
-         if map_graph.grid.has_vertex(desired_neighbour) {
-
-         }
     }
 }
 
 trait ToGridNeighbour {
-    fn to_grid_neighbour(&self) -> (i32, i32);
+    fn get_neighbour(&self, current_tile: (usize, usize)) -> (usize, usize);
 }
 
 /// This is a trait that allows us to convert a Vec2 to a grid neighbour. vec has to be normalized
-impl ToGridNeighbour for Vec2 {
-    fn to_grid_neighbour(&self) -> (i32, i32) {
-        let n = (*self).normalize();
-        let x = if n.x.is_zero() { 0 } else { n.x.signum() as i32 };
-        let y = if n.y.is_zero() { 0 } else { n.y.signum() as i32 };
-        (x, y)
+impl ToGridNeighbour for Rotation {
+    fn get_neighbour(&self, current_tile: (usize, usize)) -> (usize, usize) {
+        let n = self.0.
+            mul_vec3(Vec3::new(0.0, 0.0, -1.0))
+            .xz()
+            .normalize();
+        let x = if n.x.is_zero() { 0 } else { n.x.signum() as i32 } + current_tile.0 as i32;
+        let y = if n.y.is_zero() { 0 } else { n.y.signum() as i32 } + current_tile.1 as i32;
+        ((if x.is_negative() { 0 } else {x as usize}), if y.is_negative() { 0 } else {y as usize})
     }
 }
