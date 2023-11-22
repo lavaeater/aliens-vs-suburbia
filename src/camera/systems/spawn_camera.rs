e5d1a73e9f368f271a77f9ca3f8b58122a19b882
Tommie Nygren <tommie.nygren@gmail.com>
2023-11-22 08:33:28 +0100

    Projection

--- src/camera/systems/spawn_camera.rs
+++ src/camera/systems/spawn_camera.rs
@@ -1,16 +1,16 @@
 use bevy::core::Name;
 use bevy::math::{Quat, Vec3};
-use bevy::prelude::{Camera3dBundle, Commands, Transform};
-use bevy::prelude::Projection::Perspective;
+use bevy::prelude::{Camera3dBundle, Commands, OrthographicProjection, Transform};
+use bevy::prelude::Projection::{Orthographic, Perspective};
 use bevy::utils::default;
 use bevy_xpbd_3d::math::PI;
 use crate::camera::components::camera::{CameraOffset, GameCamera};
 pub fn spawn_camera(mut commands: Commands) {
     commands.spawn((
         Name::from("Camera"),
-        CameraOffset(Vec3::new(0.0, 10.0, 10.0)),
+        CameraOffset(Vec3::new(1.0, 1.0, 1.0)),
         Camera3dBundle {
-            projection: Perspective(Default::default()),
+            projection: Orthographic(Default::default()),
             transform: Transform {
                 rotation: Quat::from_rotation_x(-PI / 4.),
                 ..default()
