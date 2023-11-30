68e15bc9bf291eae8b517972b87a1bbad82005dd
Tommie Nygren <tommie.nygren@gmail.com>
2023-11-30 15:20:54 +0100

    I do outline in its own branch

--- src/player/player_plugin.rs
+++ src/player/player_plugin.rs
@@ -1,5 +1,8 @@
+use bevy::animation::AnimationPlayer;
 use bevy::app::{App, Plugin, Update};
-use bevy::prelude::{in_state, IntoSystemConfigs};
+use bevy::prelude::{Commands, in_state, IntoSystemConfigs, Local, Query, Res, SceneSpawner};
+use bevy::scene::SceneInstance;
+use bevy_mod_outline::InheritOutlineBundle;
 use crate::game_state::GameState;
 use crate::player::systems::spawn_players::{fix_model_transforms, spawn_players};
 
@@ -13,7 +16,30 @@ impl Plugin for PlayerPlugin {
                 (
                     spawn_players,
                     fix_model_transforms,
+                    setup_scene_once_loaded,
                 ).run_if(in_state(GameState::InGame)),
             );
+    }
+}
+
+fn setup_scene_once_loaded(
+    mut commands: Commands,
+    scene_query: Query<&SceneInstance>,
+    scene_manager: Res<SceneSpawner>,
+    mut done: Local<bool>,
+) {
+    if !*done {
+        if let Ok(scene) =
+            (scene_query.get_single())
+        {
+            if scene_manager.instance_is_ready(**scene) {
+                for entity in scene_manager.iter_instance_entities(**scene) {
+                    commands
+                        .entity(entity)
+                        .insert(InheritOutlineBundle::default());
+                }
+                *done = true;
+            }
+        }
     }
 }
