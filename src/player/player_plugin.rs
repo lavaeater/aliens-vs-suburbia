ad322f9aefab12b8a460b9d6cc18028f5e7288c4
Tommie Nygren <tommie.nygren@gmail.com>
2023-11-30 15:43:13 +0100

    Can't get it to work, because I SUCK!

--- src/player/player_plugin.rs
+++ src/player/player_plugin.rs
@@ -1,8 +1,9 @@
 use bevy::animation::AnimationPlayer;
 use bevy::app::{App, Plugin, Update};
+use bevy::DefaultPlugins;
 use bevy::prelude::{Commands, in_state, IntoSystemConfigs, Local, Query, Res, SceneSpawner};
 use bevy::scene::SceneInstance;
-use bevy_mod_outline::InheritOutlineBundle;
+use bevy_mod_outline::{AutoGenerateOutlineNormalsPlugin, InheritOutlineBundle, OutlinePlugin};
 use crate::game_state::GameState;
 use crate::player::systems::spawn_players::{fix_model_transforms, spawn_players};
 
@@ -11,6 +12,10 @@ pub struct PlayerPlugin;
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
         app
+            .add_plugins((
+                OutlinePlugin,
+                AutoGenerateOutlineNormalsPlugin,
+            ))
             .add_systems(
                 Update,
                 (
@@ -29,9 +34,7 @@ fn setup_scene_once_loaded(
     mut done: Local<bool>,
 ) {
     if !*done {
-        if let Ok(scene) =
-            (scene_query.get_single())
-        {
+        if let Ok(scene) = scene_query.get_single() {
             if scene_manager.instance_is_ready(**scene) {
                 for entity in scene_manager.iter_instance_entities(**scene) {
                     commands
--- src/player/systems/spawn_players.rs
+++ src/player/systems/spawn_players.rs
@@ -2,8 +2,9 @@ use bevy::asset::{AssetServer};
 use bevy::core::Name;
 use bevy::hierarchy::{BuildChildren, Children};
 use bevy::math::{EulerRot, Quat, Vec3};
-use bevy::prelude::{Commands, Component, Entity, EventReader, EventWriter, Query, Res, Transform, Visibility, With};
+use bevy::prelude::{Color, Commands, Component, default, Entity, EventReader, EventWriter, Query, Res, Transform, Visibility, With};
 use bevy::scene::SceneBundle;
+use bevy_mod_outline::{OutlineBundle, OutlineVolume};
 use bevy_xpbd_3d::components::{AngularDamping, Collider, CollisionLayers, Friction, LinearDamping, LockedAxes, RigidBody};
 use crate::alien::systems::spawn_aliens::{AnimationKey, CurrentAnimationKey};
 use crate::control::components::{Controller, DynamicMovement, KeyboardController};
@@ -39,53 +40,65 @@ pub fn spawn_players(
     mut player_addedd_ew: EventWriter<GameTrackingEvent>,
 ) {
     for spawn_player in spawn_player_event_reader.read() {
-        let player = commands.spawn((
-            Name::from("Player"),
-            Player {
-              key: "Player One".into()
-            },
-            FixSceneTransform::new(
-                Vec3::new(0.0, -0.35, 0.0),
-                Quat::from_euler(
-                    EulerRot::YXZ,
-                    180.0f32.to_radians(), 0.0, 0.0),
-                Vec3::new(0.5, 0.5, 0.5),
-            ),
-            KeyboardController {},
-            Controller::new(3.0, 3.0, 60.0),
-            DynamicMovement {},
-            SceneBundle {
-                scene: asset_server.load("quaternius/worker.glb#Scene0"),
-                transform: Transform::from_xyz(spawn_player.position.x, spawn_player.position.y, spawn_player.position.z),
-                //     .with_rotation(Quat::from_euler(EulerRot::YXZ, 180.0f32.to_radians(), 0.0, 0.0))
-                //     .with_scale(Vec3::new(0.25, 0.25, 0.25)),
-                ..Default::default()
-            },
-            Friction::from(0.0),
-            AngularDamping(1.0),
-            LinearDamping(0.9),
-            RigidBody::Dynamic,
-            LockedAxes::new().lock_rotation_x().lock_rotation_z(),
-            CollisionLayers::new(
-                [CollisionLayer::Player],
-                [
-                    CollisionLayer::Ball,
-                    CollisionLayer::Impassable,
-                    CollisionLayer::Floor,
-                    CollisionLayer::Alien,
-                    CollisionLayer::Player,
-                    CollisionLayer::AlienSpawnPoint,
-                    CollisionLayer::AlienGoal
-                ]),
-            Health::default(),
-            CurrentTile::default(),
-        )).insert((
-            CurrentAnimationKey::new("players".into(), AnimationKey::Walking),
-            Score::default(),
-        )).with_children(|children|
-            { // Spawn the child colliders positioned relative to the rigid body
-                children.spawn((Collider::capsule(0.4, 0.2), Transform::from_xyz(0.0, 0.0, 0.0)));
-            }).id();
+        let player =
+            commands
+                .spawn((
+                    Name::from("Player"),
+                    Player {
+                        key: "Player One".into()
+                    },
+                    FixSceneTransform::new(
+                        Vec3::new(0.0, -0.35, 0.0),
+                        Quat::from_euler(
+                            EulerRot::YXZ,
+                            180.0f32.to_radians(), 0.0, 0.0),
+                        Vec3::new(0.5, 0.5, 0.5),
+                    ),
+                    KeyboardController {},
+                    Controller::new(3.0, 3.0, 60.0),
+                    DynamicMovement {},
+                    SceneBundle {
+                        scene: asset_server.load("quaternius/worker.glb#Scene0"),
+                        transform: Transform::from_xyz(spawn_player.position.x, spawn_player.position.y, spawn_player.position.z),
+                        //     .with_rotation(Quat::from_euler(EulerRot::YXZ, 180.0f32.to_radians(), 0.0, 0.0))
+                        //     .with_scale(Vec3::new(0.25, 0.25, 0.25)),
+                        ..Default::default()
+                    },
+                    Friction::from(0.0),
+                    AngularDamping(1.0),
+                    LinearDamping(0.9),
+                    RigidBody::Dynamic,
+                    LockedAxes::new().lock_rotation_x().lock_rotation_z(),
+                    CollisionLayers::new(
+                        [CollisionLayer::Player],
+                        [
+                            CollisionLayer::Ball,
+                            CollisionLayer::Impassable,
+                            CollisionLayer::Floor,
+                            CollisionLayer::Alien,
+                            CollisionLayer::Player,
+                            CollisionLayer::AlienSpawnPoint,
+                            CollisionLayer::AlienGoal
+                        ]),
+                    Health::default(),
+                    CurrentTile::default(),
+                ))
+                .insert((
+                    CurrentAnimationKey::new("players".into(), AnimationKey::Walking),
+                    Score::default(),
+                ))
+                .insert(OutlineBundle {
+                    outline: OutlineVolume {
+                        visible: true,
+                        width: 3.0,
+                        colour: Color::RED,
+                    },
+                    ..default()
+                })
+                .with_children(|children|
+                    { // Spawn the child colliders positioned relative to the rigid body
+                        children.spawn((Collider::capsule(0.4, 0.2), Transform::from_xyz(0.0, 0.0, 0.0)));
+                    }).id();
         add_health_bar_ew.send(AddHealthBar {
             entity: player,
             name: "PLAYER",
