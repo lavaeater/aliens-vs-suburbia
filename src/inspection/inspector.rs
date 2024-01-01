5a0f13c1f42cf0c2fb8a7606ccd7bca74a3e1f7c
Tommie Nygren <tommie.nygren@gmail.com>
2024-01-01 11:01:33 +0100

    Filtered inspector egui, very nice.

--- src/inspection/inspector.rs
+++ src/inspection/inspector.rs
@@ -1,10 +1,11 @@
 use bevy::app::{App, Plugin, Update};
 use bevy::pbr::StandardMaterial;
-use bevy::prelude::{in_state, IntoSystemConfigs, With, World};
+use bevy::prelude::{Has, in_state, IntoSystemConfigs, With, World};
 use bevy::window::PrimaryWindow;
 use bevy_inspector_egui::bevy_egui::{EguiContext, EguiPlugin};
 use bevy_inspector_egui::egui;
 use crate::game_state::GameState;
+use crate::player::components::Player;
 
 pub struct InspectorPlugin;
 
@@ -27,15 +28,15 @@ fn inspector_ui(world: &mut World) {
 
     egui::Window::new("UI").show(egui_context.get_mut(), |ui| {
         egui::ScrollArea::vertical().show(ui, |ui| {
-            // equivalent to `WorldInspectorPlugin`
-            bevy_inspector_egui::bevy_inspector::ui_for_world(world, ui);
+            // // equivalent to `WorldInspectorPlugin`
+            // bevy_inspector_egui::bevy_inspector::ui_for_world(world, ui);
+            //
+            // egui::CollapsingHeader::new("Materials").show(ui, |ui| {
+            //     bevy_inspector_egui::bevy_inspector::ui_for_assets::<StandardMaterial>(world, ui);
+            // });
 
-            egui::CollapsingHeader::new("Materials").show(ui, |ui| {
-                bevy_inspector_egui::bevy_inspector::ui_for_assets::<StandardMaterial>(world, ui);
-            });
-
-            ui.heading("Entities");
-            bevy_inspector_egui::bevy_inspector::ui_for_world_entities(world, ui);
+            ui.heading("Player");
+            bevy_inspector_egui::bevy_inspector::ui_for_world_entities_filtered::<With<Player>>(world, ui,true);
         });
     });
 }
