4a0557937827d08eece081c62a15ae37bb1fb7b2
Tommie Nygren <tommie.nygren@gmail.com>
2024-01-11 18:05:10 +0100

    We need to figure out the stride, that's it basically!

--- src/generate_mesh/mod.rs
+++ src/generate_mesh/mod.rs
@@ -5,6 +5,7 @@ use bevy::prelude::*;
 use bevy::render::mesh::Indices;
 use bevy::render::mesh::VertexAttributeValues;
 use bevy::render::render_resource::PrimitiveTopology;
+use bevy::utils::petgraph::visit::Walker;
 use crate::game_state::GameState;
 
 // Define a "marker" component to mark the custom mesh. Marker components are often used in Bevy for
@@ -123,50 +124,118 @@ fn mesh_input_handler(
 
 #[rustfmt::skip]
 fn create_cube_mesh() -> Mesh {
-    let top_side = 0.5;
-    let bottom_side = 1.0;
+    let mut height = 0.0;
+    let min_x = -1.0;
+    let min_z = -1.0;
+    let max_x = 1.0;
+    let max_z = 1.0;
 
-    let mut rng = rand::thread_rng();
-    
+    /*
+    We could probably create some kind of cool mesh-generating thing pretty simply by just
+    moving along a grid and changing the height every so slightly of all the vertices. We
+    just have to remember the indices and stuff, just like before. It's not that hard, is it?
+
+    0---1
+    |f1 |
+    3---2
+
+    4---5
+    |f2 |
+    7---6
+
+
+    this is trivial stuff, mate, check the winding order.
+    0, 3, 1, 1, 3, 2,
+f1 = 0
+f2 = f1 + stride 4
+
+
+    I want to declare a plane
+    Ah, we just introduce the concept of STRIDE
+    Every square on the plane is four values and the
+
+     */
+
+    let rows = 4;
+    let columns = 5;
+    let total_squares = rows * columns;
+    let square_size = 0.5;
+    let vertices = (0..total_squares).flat_map(|i| {
+        let x = i % rows;
+        let z = i / rows;
+        let x = x as f32;
+        let z = z as f32;
+        let y = 0.0;
+        height += 0.1;
+        vec![
+            [x, y, z],
+            [x + square_size, y, z],
+            [x + square_size, y, z + square_size],
+            [x, y, z + square_size],
+        ]
+    }).collect::<Vec<[f32; 3]>>();
+
+
+    let face_map = [0, 3, 1, 1, 3, 2];
+    let indices = (0..total_squares).step_by(4).flat_map(|i|
+        [face_map[0] + i, face_map[1] + i, face_map[2] + i, face_map[3] + i, face_map[4] + i, face_map[5] + i]
+    ).collect::<Vec<u32>>();
+
+    let normals = (0..total_squares).flat_map(|_i| {
+        vec![
+            [0.0, 1.0, 0.0],
+            [0.0, 1.0, 0.0],
+            [0.0, 1.0, 0.0],
+            [0.0, 1.0, 0.0],
+        ]
+    }).collect::<Vec<[f32; 3]>>();
+
+    let uvs = (0..total_squares).step_by(4).flat_map(|i| {
+        vec![
+            [0.0, 0.25], [0.0, 0.0], [1.0, 0.0], [1.0, 0.25],
+        ]
+    }).collect::<Vec<[f32; 2]>>();
+
 
     Mesh::new(PrimitiveTopology::TriangleList)
         .with_inserted_attribute(
             Mesh::ATTRIBUTE_POSITION,
+            vertices
             // Each array is an [x, y, z] coordinate in local space.
             // Meshes always rotate around their local [0, 0, 0] when a rotation is applied to their Transform.
             // By centering our mesh around the origin, rotating the mesh preserves its center of mass.
-            vec![
-                // top (facing towards +y)
-                [-top_side, top_side, -top_side], // vertex with index 0
-                [top_side, top_side, -top_side], // vertex with index 1
-                [top_side, top_side, top_side], // etc. until 23
-                [-top_side, top_side, top_side],
-                // // bottom   (-y)
-                // [-bottom_side, -top_side, -bottom_side],
-                // [bottom_side, -top_side, -bottom_side],
-                // [bottom_side, -top_side, bottom_side],
-                // [-bottom_side, -top_side, bottom_side],
-                // // right    (+x)
-                // [top_side, -top_side, -top_side],
-                // [top_side, -top_side, top_side],
-                // [top_side, top_side, top_side], // This vertex is at the same position as vertex with index 2, but they'll have different UV and normal
-                // [top_side, top_side, -top_side],
-                // // left     (-x)
-                // [-top_side, -top_side, -top_side],
-                // [-top_side, -top_side, top_side],
-                // [-top_side, top_side, top_side],
-                // [-top_side, top_side, -top_side],
-                // // back     (+z)
-                // [-top_side, -top_side, top_side],
-                // [-top_side, top_side, top_side],
-                // [top_side, top_side, top_side],
-                // [top_side, -top_side, top_side],
-                // // forward  (-z)
-                // [-top_side, -top_side, -top_side],
-                // [-top_side, top_side, -top_side],
-                // [top_side, top_side, -top_side],
-                // [top_side, -top_side, -top_side],
-            ],
+            // vec![
+            //     // top (facing towards +y)
+            //     [min_x, height, min_z], // vertex with index 0
+            //     [max_x, height, min_z], // vertex with index 1
+            //     [max_x, height, max_z], // etc. until 23
+            //     [min_x, height, max_z],
+            //     // // bottom   (-y)
+            //     // [-bottom_side, -top_side, -bottom_side],
+            //     // [bottom_side, -top_side, -bottom_side],
+            //     // [bottom_side, -top_side, bottom_side],
+            //     // [-bottom_side, -top_side, bottom_side],
+            //     // // right    (+x)
+            //     // [top_side, -top_side, -top_side],
+            //     // [top_side, -top_side, top_side],
+            //     // [top_side, top_side, top_side], // This vertex is at the same position as vertex with index 2, but they'll have different UV and normal
+            //     // [top_side, top_side, -top_side],
+            //     // // left     (-x)
+            //     // [-top_side, -top_side, -top_side],
+            //     // [-top_side, -top_side, top_side],
+            //     // [-top_side, top_side, top_side],
+            //     // [-top_side, top_side, -top_side],
+            //     // // back     (+z)
+            //     // [-top_side, -top_side, top_side],
+            //     // [-top_side, top_side, top_side],
+            //     // [top_side, top_side, top_side],
+            //     // [top_side, -top_side, top_side],
+            //     // // forward  (-z)
+            //     // [-top_side, -top_side, -top_side],
+            //     // [-top_side, top_side, -top_side],
+            //     // [top_side, top_side, -top_side],
+            //     // [top_side, -top_side, -top_side],
+            // ],
         )
         // Set-up UV coordinated to point to the upper (V < 0.5), "dirt+grass" part of the texture.
         // Take a look at the custom image (assets/textures/array_texture.png)
@@ -174,20 +243,21 @@ fn create_cube_mesh() -> Mesh {
         // Note: (0.0, 0.0) = Top-Left in UV mapping, (1.0, 1.0) = Bottom-Right in UV mapping
         .with_inserted_attribute(
             Mesh::ATTRIBUTE_UV_0,
-            vec![
-                // Assigning the UV coords for the top side.
-                [0.0, 0.2], [0.0, 0.0], [1.0, 0.0], [1.0, 0.25],
-                // Assigning the UV coords for the bottom side.
-                // [0.0, 0.45], [0.0, 0.25], [1.0, 0.25], [1.0, 0.45],
-                // // Assigning the UV coords for the right side.
-                // [1.0, 0.45], [0.0, 0.45], [0.0, 0.2], [1.0, 0.2],
-                // // Assigning the UV coords for the left side.
-                // [1.0, 0.45], [0.0, 0.45], [0.0, 0.2], [1.0, 0.2],
-                // // Assigning the UV coords for the back side.
-                // [0.0, 0.45], [0.0, 0.2], [1.0, 0.2], [1.0, 0.45],
-                // // Assigning the UV coords for the forward side.
-                // [0.0, 0.45], [0.0, 0.2], [1.0, 0.2], [1.0, 0.45],
-            ],
+            uvs,
+            // vec![
+            //     // Assigning the UV coords for the top side.
+            //     [0.0, 0.25], [0.0, 0.0], [1.0, 0.0], [1.0, 0.25],
+            //     // Assigning the UV coords for the bottom side.
+            //     // [0.0, 0.45], [0.0, 0.25], [1.0, 0.25], [1.0, 0.45],
+            //     // // Assigning the UV coords for the right side.
+            //     // [1.0, 0.45], [0.0, 0.45], [0.0, 0.2], [1.0, 0.2],
+            //     // // Assigning the UV coords for the left side.
+            //     // [1.0, 0.45], [0.0, 0.45], [0.0, 0.2], [1.0, 0.2],
+            //     // // Assigning the UV coords for the back side.
+            //     // [0.0, 0.45], [0.0, 0.2], [1.0, 0.2], [1.0, 0.45],
+            //     // // Assigning the UV coords for the forward side.
+            //     // [0.0, 0.45], [0.0, 0.2], [1.0, 0.2], [1.0, 0.45],
+            // ],
         )
         // For meshes with flat shading, normals are orthogonal (pointing out) from the direction of
         // the surface.
@@ -195,38 +265,39 @@ fn create_cube_mesh() -> Mesh {
         // Each array represents a normalized vector, which length should be equal to 1.0.
         .with_inserted_attribute(
             Mesh::ATTRIBUTE_NORMAL,
-            vec![
-                // Normals for the top side (towards +y)
-                [0.0, 1.0, 0.0],
-                [0.0, 1.0, 0.0],
-                [0.0, 1.0, 0.0],
-                [0.0, 1.0, 0.0],
-                // // Normals for the bottom side (towards -y)
-                // [0.0, -1.0, 0.0],
-                // [0.0, -1.0, 0.0],
-                // [0.0, -1.0, 0.0],
-                // [0.0, -1.0, 0.0],
-                // // Normals for the right side (towards +x)
-                // [1.0, 0.0, 0.0],
-                // [1.0, 0.0, 0.0],
-                // [1.0, 0.0, 0.0],
-                // [1.0, 0.0, 0.0],
-                // // Normals for the left side (towards -x)
-                // [-1.0, 0.0, 0.0],
-                // [-1.0, 0.0, 0.0],
-                // [-1.0, 0.0, 0.0],
-                // [-1.0, 0.0, 0.0],
-                // // Normals for the back side (towards +z)
-                // [0.0, 0.0, 1.0],
-                // [0.0, 0.0, 1.0],
-                // [0.0, 0.0, 1.0],
-                // [0.0, 0.0, 1.0],
-                // // Normals for the forward side (towards -z)
-                // [0.0, 0.0, -1.0],
-                // [0.0, 0.0, -1.0],
-                // [0.0, 0.0, -1.0],
-                // [0.0, 0.0, -1.0],
-            ],
+            normals
+            // vec![
+            //     // Normals for the top side (towards +y)
+            //     [0.0, 1.0, 0.0],
+            //     [0.0, 1.0, 0.0],
+            //     [0.0, 1.0, 0.0],
+            //     [0.0, 1.0, 0.0],
+            //     // // Normals for the bottom side (towards -y)
+            //     // [0.0, -1.0, 0.0],
+            //     // [0.0, -1.0, 0.0],
+            //     // [0.0, -1.0, 0.0],
+            //     // [0.0, -1.0, 0.0],
+            //     // // Normals for the right side (towards +x)
+            //     // [1.0, 0.0, 0.0],
+            //     // [1.0, 0.0, 0.0],
+            //     // [1.0, 0.0, 0.0],
+            //     // [1.0, 0.0, 0.0],
+            //     // // Normals for the left side (towards -x)
+            //     // [-1.0, 0.0, 0.0],
+            //     // [-1.0, 0.0, 0.0],
+            //     // [-1.0, 0.0, 0.0],
+            //     // [-1.0, 0.0, 0.0],
+            //     // // Normals for the back side (towards +z)
+            //     // [0.0, 0.0, 1.0],
+            //     // [0.0, 0.0, 1.0],
+            //     // [0.0, 0.0, 1.0],
+            //     // [0.0, 0.0, 1.0],
+            //     // // Normals for the forward side (towards -z)
+            //     // [0.0, 0.0, -1.0],
+            //     // [0.0, 0.0, -1.0],
+            //     // [0.0, 0.0, -1.0],
+            //     // [0.0, 0.0, -1.0],
+            // ],
         )
         // Create the triangles out of the 24 vertices we created.
         // To construct a square, we need 2 triangles, therefore 12 triangles in total.
@@ -235,14 +306,17 @@ fn create_cube_mesh() -> Mesh {
         // should appear counter-clockwise from the front of the triangle, in this case from outside the cube).
         // Read more about how to correctly build a mesh manually in the Bevy documentation of a Mesh,
         // further examples and the implementation of the built-in shapes.
-        .with_indices(Some(Indices::U32(vec![
-            0, 3, 1, 1, 3, 2, // triangles making up the top (+y) facing side.
+        .with_indices(Some(Indices::U32(
+            indices
+            // vec![
+            // 0, 3, 1, 1, 3, 2, // triangles making up the top (+y) facing side.
             // 4, 5, 7, 5, 6, 7, // bottom (-y)
             // 8, 11, 9, 9, 11, 10, // right (+x)
             // 12, 13, 15, 13, 14, 15, // left (-x)
             // 16, 19, 17, 17, 19, 18, // back (+z)
             // 20, 21, 23, 21, 22, 23, // forward (-z)
-        ])))
+        // ]
+        )))
 }
 
 // Function that changes the UV mapping of the mesh, to apply the other texture.
