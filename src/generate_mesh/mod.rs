0e66a6971f8ab0eccfb1f0b27e40f74fd730bc02
Tommie Nygren <tommie.nygren@gmail.com>
2024-01-14 16:29:01 +0100

    What the actual funyon

--- src/generate_mesh/mod.rs
+++ src/generate_mesh/mod.rs
@@ -1,3 +1,4 @@
+use std::io::Error;
 // ! This example demonstrates how to create a custom mesh,
 // ! assign a custom UV mapping for a custom texture,
 // ! and how to change the UV mapping at run-time.
@@ -5,6 +6,8 @@ use bevy::prelude::*;
 use bevy::render::mesh::Indices;
 use bevy::render::mesh::VertexAttributeValues;
 use bevy::render::render_resource::PrimitiveTopology;
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
+use image::{ImageBuffer, Luma};
 use crate::game_state::GameState;
 
 // Define a "marker" component to mark the custom mesh. Marker components are often used in Bevy for
@@ -18,6 +21,7 @@ impl Plugin for MeshPlugin {
     fn build(&self, app: &mut App) {
         app
             .add_systems(OnEnter(GameState::Mesh), setup_mesh)
+            .add_plugins(WorldIynspectorPlugin::new())
             .add_systems(Update, (mesh_input_handler).run_if(in_state(GameState::Mesh)));
     }
 }
@@ -31,7 +35,7 @@ fn setup_mesh(
     // Import the custom texture.
     let custom_texture_handle: Handle<Image> = asset_server.load("textures/array_texture.png");
     // Create and save a handle to the mesh.
-    let cube_mesh_handle: Handle<Mesh> = meshes.add(create_cube_mesh());
+    let cube_mesh_handle: Handle<Mesh> = meshes.add(load_terrain_mesh().unwrap());
 
     // Render the mesh with the custom texture using a PbrBundle, add the marker.
     commands.spawn((
@@ -314,4 +318,106 @@ fn toggle_texture(mesh_to_change: &mut Mesh) {
             uv_coord[1] -= 0.5;
         }
     }
+}
+
+fn sample_vertex_height(cy: i32, cx: i32, heightmap: &ImageBuffer<Luma<u16>, Vec::<u16>>) -> f32 {
+    let mut cnt = 0;
+    let mut height = 0.0;
+
+    for dy in [-1, 0].iter() {
+        for dx in [-1, 0].iter() {
+            let sy = cy + dy;
+            let sx = cx + dx;
+            if    sy < 0
+                || sx < 0
+                || sy >= heightmap.height() as i32
+                || sx >= heightmap.width() as i32 {
+                continue;
+            } else {
+                height += heightmap.get_pixel(
+                    sx as u32, sy as u32).0[0] as f32 * 1.0f32 / std::u16::MAX as f32;
+                cnt += 1;
+            }
+        }
+    }
+
+    height / cnt as f32
+}
+
+fn load_terrain_mesh() -> Result<Mesh, Error> {
+    let filename = "assets/terrain.png";
+
+    let side_length = 1f32;
+    let max_height = 1f32;
+    let terrain_bitmap = image::io::Reader::open(filename).unwrap().decode().unwrap();
+    let mut mesh = Mesh::new(PrimitiveTopology::TriangleList);
+
+    let heightmap = terrain_bitmap.as_luma16().unwrap();
+
+    let mut vertices : Vec::<[f32; 3]> = Vec::new();
+    let mut normals : Vec::<[f32; 3]> = Vec::new();
+    let mut indices : Vec::<u32> = Vec::new();
+
+    let vertex_number = ( (heightmap.height() + 1) *
+        (heightmap.width() + 1) ) as usize;
+
+    vertices.resize(vertex_number, [0.0f32, 0.0f32, 0.0f32]);
+    normals.resize(vertex_number, [0.0f32, 1.0f32, 0.0f32]);
+    let uvs = vec![[0.0, 0.0]; vertices.len()];
+
+
+    let mut vertex_index = 0;
+    for cy in 0..(heightmap.height() as i32 +1) {
+        for cx in 0..(heightmap.width() as i32 +1) {
+            let height = sample_vertex_height(cy, cx, heightmap);
+            // println!("sampled height at y={:>3} x={:>3}  = {:>4}", cy, cx, height);
+
+            vertices[vertex_index] = [cx as f32 * side_length,
+                height * max_height,
+                cy as f32 * side_length];
+            vertex_index += 1;
+        }
+    }
+
+    let grid_height = heightmap.height() + 1;
+    let grid_width = heightmap.width() + 1;
+
+    for cy in 0..(heightmap.height()) {
+        for cx in 0..(heightmap.width()) {
+            indices.extend([
+                cy * grid_width + cx,
+                (cy + 1) * grid_width + cx + 1,
+                cy * grid_width + cx + 1,
+            ].iter());
+            indices.extend([
+                cy * grid_width + cx,
+                (cy + 1) * grid_width + cx,
+                (cy + 1) * grid_width + cx + 1,
+            ].iter());
+        }
+    }
+
+    // for i in 0..(indices.len()/3) {
+    //     println!("triangle {:03}: {} {} {} ",
+    //         i, indices[i*3], indices[i*3+1], indices[i*3+2])
+    // }
+
+    // println!(" {} {} ", indices.len() / 3, 2  * heightmap.height() * (heightmap.width()));
+
+    assert!(indices.len() as u32 /  3 == 2  * heightmap.height() * (heightmap.width()) );
+
+
+    mesh.insert_attribute(
+        Mesh::ATTRIBUTE_POSITION,
+        VertexAttributeValues::Float32x3(vertices));
+    mesh.insert_attribute(
+        Mesh::ATTRIBUTE_NORMAL,
+        VertexAttributeValues::Float32x3(normals));
+    mesh.insert_attribute(
+        Mesh::ATTRIBUTE_UV_0,
+        VertexAttributeValues::Float32x2(uvs));
+    mesh.set_indices(Some(Indices::U32(indices)));
+
+
+    Ok(mesh)
 }
